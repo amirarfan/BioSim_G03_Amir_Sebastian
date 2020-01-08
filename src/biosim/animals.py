9fbccfc6ca7c62889528c2133e8b57271b284c68
Sebastian Becker <sebabeck@nmbu.no>
2020-01-08 13:24:27 +0100

    Added parameters and compute_kill_prob

--- src/biosim/animals.py
+++ src/biosim/animals.py
@@ -61,15 +61,51 @@ class Animal:
 
 
     class Herbivore(Animal):
-        # Parameterene til Herbivore
+        param ={"weight_birth":8.0,
+                "sigma_birth":1.5,
+                "beta":0.9,
+                "eta":0.05,
+                "a_half":40.0,
+                "phi_age":0.2,
+                "w_half":10.0,
+                "phi_wheight":0.1,
+                "mu":0.25,
+                "lambda":1.0,
+                "gamma":0.2,
+                "zeta":3.5,
+                "xi":1.2,
+                "omega":0.4,
+                "Fodder":10.0,
+                "deltaPhimax":0}
         def __init__(self, weight=None, age=None):
-            pass # Super
+            pass # Super location
 
     class Carnivore(Animal):
-        # Parameterne til Carnivore
+        param = {"weight_birth": 6.0,
+                 "sigma_birth": 1.0,
+                 "beta": 0.75,
+                 "eta": 0.125,
+                 "a_half": 60.0,
+                 "phi_age": 0.4,
+                 "w_half": 4.0,
+                 "phi_wheight": 0.4,
+                 "mu": 0.4,
+                 "lambda": 1.0,
+                 "gamma": 0.8,
+                 "zeta": 3.5,
+                 "xi": 1.1,
+                 "omega": 0.9,
+                 "Fodder": 50.0,
+                 "deltaPhimax": 10.0}
+
         def __init__(self, weight=None, age=None):
             # IF none sett til standard verdi
             pass # Super
 
-        def kill(self):
+        @classmethod
+        def compute_kill_prob(cls,min_fit_kill):
+            #if
+            pass
+
+        def kill(self,min_fit_kill):
             pass
