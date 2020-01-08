cc608f4923a06b724c3c4cbcc4c8d334a6181e77
Amir Arfan <amirarfan7@gmail.com>
2020-01-08 13:37:59 +0100

    Added subclasses of the different areas on the map

--- src/biosim/map.py
+++ src/biosim/map.py
@@ -1,6 +1,30 @@
-class atlas:
+class Atlas:
     def __init__(self, map):
         self.map = map
+
     def generate_fodder(self):
         pass
-    
+
+
+class OceanMountain(Atlas):
+    def __init__(self):
+        pass
+
+class Desert(Atlas):
+    # Herbivores kan ikke spise her, dvs at hvis de spiser er det feil
+    def __init__(self):
+        pass
+
+class Savannah(Atlas):
+    def __init__(self, fodder_max):
+        self.fodder_max = fodder_max
+        pass
+
+class Jungle(Atlas):
+    def __init__(self, fodder_max):
+        self.fodder_max = fodder_max
+        pass
+
+
+
+
