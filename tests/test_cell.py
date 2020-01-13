0d031746c689a3f01f834fa0dd0fa55461c8607b
Amir Arfan <amirarfan7@gmail.com>
2020-01-13 10:06:09 +0100

    Removed some tests

--- tests/test_cell.py
+++ tests/test_cell.py
@@ -10,26 +10,19 @@ def test_fodder_Mountain():
     mountain_cell = cell.Mountain()
     assert mountain_cell.current_fodder == 0
 
-
-def test_add_animal():
-    cell.Cell
-    assert ValueError  # Hva er hensikten her??
-
-
 def test_fodder_Ocean():
     ocean_cell = cell.Ocean()
-    assert ocean_cell.relevant_fodder == 0
+    assert ocean_cell.current_fodder == 0
 
 
 def test_gen_fodder_jung():
     jun_cell = cell.Jungle()
-    assert jun_cell.current_fodder == fodder.param["f_max"]
-    assert (
-        jun_cell.current_fodder != 0
-    )  # Denne blir unødvendig da du allerede tester at det er en verdi
+    jun_cell.current_fodder = 700
+    jun_cell.gen_fodder_jung()
+    assert jun_cell.current_fodder == jun_cell.param["f_max"]
+
 
 
 def test_gen_fodder_Savannah():
     savannah_cell = cell.Savannah()
     assert savannah_cell.current_fodder == 300
-    assert savannah_cell.current_fodder != None  # Denne også
