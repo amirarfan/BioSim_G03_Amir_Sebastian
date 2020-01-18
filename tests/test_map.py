b3bfd6400041d5328597ed9868192b1a27d76942
Amir Arfan <amirarfan7@gmail.com>
2020-01-18 21:37:37 +0100

    Added tests for map.py

--- tests/test_map.py
+++ tests/test_map.py
@@ -2,3 +2,56 @@
 
 __author__ = "Amir Arfan, Sebastian Becker"
 __email__ = "amar@nmbu.no"
+
+from biosim.map import Map
+from biosim.cell import Mountain, Ocean, Savannah, Jungle, Desert
+from biosim.animals import Herbivore, Carnivore
+import pytest
+import textwrap
+
+
+@pytest.fixture
+def standard_map():
+    sgeogr = """\
+               OOOOOOOOOOOOOOOOOOOOO
+               OOOOOOOOSMMMMJJJJJJJO
+               OSSSSSJJJJMMJJJJJJJOO
+               OSSSSSSSSSMMJJJJJJOOO
+               OSSSSSJJJJJJJJJJJJOOO
+               OSSSSSJJJDDJJJSJJJOOO
+               OSSJJJJJDDDJJJSSSSOOO
+               OOSSSSJJJDDJJJSOOOOOO
+               OSSSJJJJJDDJJJJJJJOOO
+               OSSSSJJJJDDJJJJOOOOOO
+               OOSSSSJJJJJJJJOOOOOOO
+               OOOSSSSJJJJJJJOOOOOOO
+               OOOOOOOOOOOOOOOOOOOOO"""
+
+    sgeogr = textwrap.dedent(sgeogr)
+
+    return sgeogr
+
+
+def test_constructor_map(standard_map):
+    """
+    Tests the constructor in Map
+    Parameters
+    ----------
+    standard_map
+
+    Returns
+    -------
+
+    """
+    test_map = Map(standard_map)
+    assert isinstance(test_map, Map)
+
+
+def test_uneven_map():
+    test_map = "OOO\nODSDO\nOOO"
+    with pytest.raises(ValueError):
+        Map(test_map)
+
+def test_non_allowed_cell_type():
+    test_map = "OOO\nOKO\nOOO"
+    with pytes.raises(ValueError)
