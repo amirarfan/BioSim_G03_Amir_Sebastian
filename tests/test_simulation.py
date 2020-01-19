7ff7ff88492076959cdbdab9e6476dfb52729adf
Amir Arfan <amirarfan7@gmail.com>
2020-01-19 14:28:46 +0100

    Started working on test simulation

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -1,4 +1,56 @@
 # -*- coding: utf-8 -*-
 
-__author__ = ""
-__email__ = "@nmbu.no"
+__author__ = "Amir Arfan, Sebastian Becker"
+__email__ = "amar@nmbu.no, sebabeck@nmbu.no"
+
+import pytest
+from biosim.simulation import BioSim
+import textwrap
+import os
+
+
+@pytest.fixture
+def standard_sim():
+    geogr = """\
+                   OOOOOOOOOOOOOOOOOOOOO
+                   OOOOOOOOSMMMMJJJJJJJO
+                   OSSSSSJJJJMMJJJJJJJOO
+                   OSSSSSSSSSMMJJJJJJOOO
+                   OSSSSSJJJJJJJJJJJJOOO
+                   OSSSSSJJJDDJJJSJJJOOO
+                   OSSJJJJJDDDJJJSSSSOOO
+                   OOSSSSJJJDDJJJSOOOOOO
+                   OSSSJJJJJDDJJJJJJJOOO
+                   OSSSSJJJJDDJJJJOOOOOO
+                   OOSSSSJJJJJJJJOOOOOOO
+                   OOOSSSSJJJJJJJOOOOOOO
+                   OOOOOOOOOOOOOOOOOOOOO"""
+    geogr = textwrap.dedent(geogr)
+
+    ini_herbs = [
+        {
+            "loc": (10, 10),
+            "pop": [
+                {"species": "Herbivore", "age": 5, "weight": 20}
+                for _ in range(150)
+            ],
+        }
+    ]
+    ini_carns = [
+        {
+            "loc": (10, 10),
+            "pop": [
+                {"species": "Carnivore", "age": 5, "weight": 20}
+                for _ in range(40)
+            ],
+        }
+    ]
+    return BioSim(geogr, ini_herbs, seed=123, img_base=".dv")
+
+
+def test_standard_biosim(standard_sim):
+    standard_sim.simulate(100)
+
+
+
+
